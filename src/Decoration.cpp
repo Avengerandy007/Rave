b119efd59fd4b8ffb2064f96c7971a92b5df7f38
Avengerandy007 <157376984+Avengerandy007@users.noreply.github.com>
2025-11-07 21:05:39 +0200

    Decorations were being generating on screen because player.direction.X/Y = 0

--- src/Decoration.cpp
+++ src/Decoration.cpp
@@ -26,10 +26,10 @@ void Decoration::Regenerate(){
 	//check if distance between this and player is larger than the resolution on X
 	if (std::abs(player->position.X - position.X) > camera->resolutionX){
 		//make sure that the player's direction is positive so the random minimum is smaller than the max
-		if (player->direction.X > 0){
-			random.ChangeRange((int)(player->position.X + player->direction.X * camera->resolutionX), (int)(player->position.X  + player->direction.X * (camera->resolutionX + 300)));
+		if (player->direction.X >= 0){
+			random.ChangeRange((int)(player->position.X + camera->resolutionX), (int)(player->position.X  + (camera->resolutionX + 300)));
 		}else{
-			random.ChangeRange((int)(player->position.X + player->direction.X * (camera->resolutionX + 300)), (int)(player->position.X  + player->direction.X * camera->resolutionX));
+			random.ChangeRange((int)(player->position.X + -1 * (camera->resolutionX + 300)), (int)(player->position.X  + -1 * camera->resolutionX));
 		}
 		position.X = random.GetRandomNumber();
 		random.ChangeRange(0, 1);
@@ -38,10 +38,10 @@ void Decoration::Regenerate(){
 	//check if distance between this and player is larger than the resolution on Y
 	if (std::abs(player->position.Y - position.Y) > camera->resolutionY){
 		//make sure that the player's direction is positive so the random minimum is smaller than the max
-		if (player->direction.Y > 0) {
-			random.ChangeRange((int)(player->position.Y + player->direction.Y * camera->resolutionY), (int)(player->position.Y + player->direction.Y * (camera->resolutionY + 300)));
+		if (player->direction.Y >= 0) {
+			random.ChangeRange((int)(player->position.Y +  camera->resolutionY), (int)(player->position.Y +  (camera->resolutionY + 300)));
 		} else {
-			random.ChangeRange((int)(player->position.Y + player->direction.Y * (camera->resolutionY + 300)), (int)(player->position.Y + player->direction.Y * camera->resolutionY));
+			random.ChangeRange((int)(player->position.Y + -1 * (camera->resolutionY + 300)), (int)(player->position.Y + -1 * camera->resolutionY));
 		}
 		position.Y = random.GetRandomNumber();
 		random.ChangeRange(0, 1);
