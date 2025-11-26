9a2619e1ead98134c5826b82c0b7ae68fde97e84
Avengerandy007 <157376984+Avengerandy007@users.noreply.github.com>
2025-11-26 21:30:32 +0200

    Get unit vector of Projectile::targetDirection and use that instead

--- src/Projectile.cpp
+++ src/Projectile.cpp
@@ -2,6 +2,7 @@
 
 Projectile::Projectile(Projectile::Types t, GameFr::Vector2& target) : type(t), targetDirection(target), random(4, 7){
 	speed = random.GetRandomNumber();
+	targetDirection.Normalize();
 }
 
 void Projectile::Update(){
