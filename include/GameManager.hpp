600d56fec92ef8c5b10f8b0b0052494472d5c976
Avengerandy007 <157376984+Avengerandy007@users.noreply.github.com>
2025-12-05 23:44:22 +0200

    Removed broken engine component

--- include/GameManager.hpp
+++ include/GameManager.hpp
@@ -17,7 +17,6 @@ public:
 	std::shared_ptr<GameFr::Camera2D> camera;
 	Util::ObjectArray<Global::nrOfDecorations> decorations;
 	Util::ObjectArray<Global::nrOfEnemies> enemies;
-	GameFr::Util::CollisionManager collisionManager;
 	ProjectileFactory projectileFactory;
 	void Update();
 	GameManager();
