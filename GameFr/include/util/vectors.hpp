b02b9c2ce8c66e49ebe5cd041b7d30eb58d8b294
Avengerandy007 <157376984+Avengerandy007@users.noreply.github.com>
2025-12-22 14:57:34 +0200

    Updated GameFr

--- GameFr/include/util/vectors.hpp
+++ GameFr/include/util/vectors.hpp
@@ -8,6 +8,12 @@ namespace GameFr{
 		Vector2();
 
 		friend bool operator==(const Vector2& l, const Vector2& r);
+		friend Vector2 operator+(const Vector2& a, const float& b);
+		friend Vector2 operator+(const Vector2& a, const Vector2& b);
+		friend Vector2 operator-(const Vector2& a, const float& b);
+		friend Vector2 operator-(const Vector2& a, const Vector2& b);
+		friend Vector2 operator*(const Vector2& a, const float& b);
+		friend Vector2 operator/(const Vector2& a, const float& b);
 
 		static float GetDistance(const Vector2& first, const Vector2& second);
 		static Vector2 GetDiference(const Vector2& first, const Vector2& second);
