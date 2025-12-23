1db64e8153384f1dd172b27c8114ab3db96379d1
Avengerandy007 <157376984+Avengerandy007@users.noreply.github.com>
2025-12-23 23:47:29 +0200

    Implemented gif player as per #7

--- include/util/Texture.hpp
+++ include/util/Texture.hpp
@@ -1,17 +1,35 @@
 #pragma once
 #include <string>
 #include "raylib.h"
+#include <inttypes.h>
 
 namespace Util{
 	class Texture{
+	protected:
 		Image image;
 	public:
 		Texture2D texture;
 		Texture(std::string path);
 		Texture();
 		Texture(const Texture& other);
-		void Assign(std::string path);
-		void Assign(const Image& img);
-		void Unload();
+		virtual void Assign(const std::string path);
+		virtual void Assign(const Image& img);
+		virtual void Unload();
+	};
+
+	class AnimatedTexture : public Texture{
+		uint32_t frame;
+		uint32_t frameDelay;
+		uint32_t frameCounter;
+		int totalFrames;
+		int frameOffset;
+	public:
+		AnimatedTexture(const std::string path);
+		AnimatedTexture();
+		AnimatedTexture(const AnimatedTexture& other);
+		void Assign(const std::string path) override;
+		void Assign(const Image& img) override;
+		void Update();
+		void Unload() override;
 	};
 }
--- src/util/Texture.cpp
+++ src/util/Texture.cpp
@@ -33,4 +33,52 @@ namespace Util{
 			texture.id = 0;
 		}
 	}
+
+
+	AnimatedTexture::AnimatedTexture(const std::string path){
+		image = LoadImageAnim(path.c_str(), &totalFrames);
+		texture = LoadTextureFromImage(image);
+		frameDelay = 1;
+	}
+
+	AnimatedTexture::AnimatedTexture(){
+		std::clog << "Animated texture default ctor\n";
+	}
+
+	AnimatedTexture::AnimatedTexture(const AnimatedTexture& other){
+		frame = other.frame;
+		frameDelay = other.frameDelay;
+		image = other.image;
+		texture = other.texture;
+	}
+
+	void AnimatedTexture::Assign(const std::string path){
+		image = LoadImageAnim(path.c_str(), &totalFrames);
+		texture = LoadTextureFromImage(image);
+	}
+	
+	void AnimatedTexture::Assign(const Image& img){
+		image = img;
+	}
+
+	void AnimatedTexture::Update(){
+		frameCounter++;
+		if (frameCounter >= frameDelay){
+			frameCounter = 0;;
+			frame++;
+			if (frame >= totalFrames) frame = 0;
+			frameOffset = image.width * image.height * 4 * frame;
+			UpdateTexture(texture, (unsigned char *)image.data + frameOffset);
+		}
+	}
+
+	void AnimatedTexture::Unload(){
+		if (image.data){
+			UnloadImage(image);
+		}
+		if (texture.id != 0){
+			UnloadTexture(texture);
+			texture.id = 0;
+		}
+	}
 }
