84b48687e3da70052d1f36aed60b478e6c8f1d26
Avengerandy007 <157376984+Avengerandy007@users.noreply.github.com>
2025-12-31 22:18:11 +0200

    Updated dependency

--- GameFr/include/event.hpp
+++ GameFr/include/event.hpp
@@ -3,6 +3,7 @@
 #include <cstdint>
 #include "util/vectors.hpp"
 #include <memory>
+#include "util/SmartCounter.hpp"
 
 namespace GameFr{
 class Entity2D;
@@ -18,30 +19,36 @@ class Entity2D;
 
 	struct Event{
 		enum struct Types;
-		const Types type;
-		const std::shared_ptr<const Entity2D> sender;
-		const std::shared_ptr<const Entity2D> receiver;
-		const Util::EventDataPoint dataPoint;
+		const Types type;				//do I really need to explain this?
+		const std::shared_ptr<const Entity2D> sender; 	//object who created this
+		const std::shared_ptr<const Entity2D> receiver; //object for whom it's meant for
+		const Util::EventDataPoint dataPoint;		//additional data to send along
 		Event(const Types t, const std::shared_ptr<const Entity2D> s, const std::shared_ptr<const Entity2D> r, const Util::EventDataPoint d);
 	};
 
 	struct EventQueue{
 		std::array<std::shared_ptr<const Event>, 10000> queue;
-		void CreateEvent(const std::shared_ptr<const Event>& event);
-		uint64_t qp = 0;
+		void CreateEvent(const std::shared_ptr<const Event>& event); //append an event object ptr at qp in queue
+		Util::SmartCounter<uint16_t, 0, 9999, 1> qp;
 	};
 
+	//Object to interface to queue
 	class EventInterface{
-		uint64_t qp = 0;
+		//queue ptr (where should it start iterating from on Listen)
+		Util::SmartCounter<uint16_t, 0, 9999, 1> qp;
 	public:
-		std::shared_ptr<EventQueue> queue;
+		std::shared_ptr<EventQueue> queue;	//ptr to assigned queue
 		EventInterface();
 		EventInterface(const EventInterface& other);
-		std::shared_ptr<const Event> Listen(const std::shared_ptr<const Entity2D> parent);
-		std::shared_ptr<const Event> Listen(const Event::Types desiredType);
+
+		//iterates over elements >= qp and <= queue->qp in queue->queue, returns nullptr if nothing is foun
+		std::shared_ptr<const Event> Listen(const std::shared_ptr<const Entity2D> parent); //returns first element to match parent argument in Event::receiver
+		std::shared_ptr<const Event> Listen(const Event::Types desiredType); //returns first element to match desired type
+
 		void AssignQueue(const std::shared_ptr<EventQueue> q);
 	};
 
+	//You can add new event types here
 	enum struct Event::Types{
 		COLLISION,
 		SHOOT,
